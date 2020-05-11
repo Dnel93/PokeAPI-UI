62014128283a2350a3854a4ee768e9f6790c9ef8
nikedanz <dvillarreal93@gmail.com>
2020-05-10 20:17:57 -0500

    fix: fixed css issue on Leftbar

--- src/components/Leftbar/Leftbar.js
+++ src/components/Leftbar/Leftbar.js
@@ -19,17 +19,19 @@ function Leftbar() {
 
     return(
         <div className="col-2 h-100 py-2 d-flex justify-content-center fixed-top Leftbar">
-            <h1 className="Leftbar-Header">PokeAPI</h1>
-            <hr/>
             <div>
-                <form onSubmit={handleOnSubmit}>
-                    <div className="form-group">
-                        <input type="text" className="form-control bg-dark text-light" onKeyDown={handleKeyPress} placeholder="Pikachu, Entei..."/>
-                    </div>
-                </form>
+                <h1 className="Leftbar-Header">PokeAPI</h1>
+                <hr/>
+                <div>
+                    <form onSubmit={handleOnSubmit}>
+                        <div className="form-group">
+                            <input type="text" className="form-control bg-dark text-light" onKeyDown={handleKeyPress} placeholder="Pikachu, Entei..."/>
+                        </div>
+                    </form>
+                </div>
+                <hr />
+                <a className="twitter-account" href="https://twitter.com/nikedanz">@Nikedanz</a>
             </div>
-            <hr />
-            <a className="twitter-account" href="https://twitter.com/nikedanz">@Nikedanz</a>
         </div>
     );
 }
